9a766079778a11083f25f07fdf7a0df5f328720f
Aaron A Dsouza <aarondsouza0612@gmail.com>
2025-02-19 11:06:45 +0530

    Updated till project 9

---
+++ 09themeswitcher/src/contexts/theme.js
@@ -0,0 +1,28 @@
+// import { createContext, useContext } from "react";
+
+// export const ThemeContext = createContext({
+//     themeMode : "light",
+//     darkTheme : () => {},
+//     lightTheme : () => {},
+// });
+
+// export const ThemeProvider = ThemeContext.Provider;
+
+// export default function useTheme() {
+//     return useContext(ThemeContext);
+// }
+
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
